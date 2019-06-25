3c53d34b7ad09ec3ab6bd8ae5a3d1d0ec901171a
Antony Guinard <antony.guinard@gmail.com>
2019-06-25 23:53:25 +0100

    Wire up search functionality and make the search workflow work nicely

--- src/checkout/Checkout.js
+++ src/checkout/Checkout.js
@@ -63,24 +63,24 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const steps = ['Shipping address', 'Payment details', 'Review your order'];
+const steps = ['Nom(s)', 'Ajouter livres', 'Livres empruntés'];
 
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return <AddressForm />;
-    case 1:
-      return <PaymentForm />;
-    case 2:
-      return <Review />;
-    default:
-      throw new Error('Unknown step');
-  }
-}
+// let myState = { name: "Yo!"}
+
+
+
+
 
 export default function Checkout() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
+  const [name, setName] = React.useState("Yo!");
+  const [email, setEmail] = React.useState("");
+  const [books, setBooks] = React.useState([
+    {ISBN: "ISBN1", title: "Book title 1", imageUrl: "https://pictures.abebooks.com/isbn/9780321543257-us-300.jpg"}, 
+    {ISBN: "ISBN2", title: "Book title 2", imageUrl: "https://pictures.abebooks.com/isbn/9780857809704-us-300.jpg"}
+  ]);
+  const [fetching, setFetching] = React.useState(false);
 
   const handleNext = () => {
     setActiveStep(activeStep + 1);
@@ -90,20 +90,71 @@ export default function Checkout() {
     setActiveStep(activeStep - 1);
   };
 
+  const searchBook = (isbn) => {
+    const indexToUpdate = books.length;
+    setFetching(true);
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            console.log("Response: ", xhr.responseText);
+            updateBook(JSON.parse(xhr.responseText), indexToUpdate)
+            setFetching(false);
+        }
+        // TODO: Handle failure or bad result???
+    }
+    xhr.open('GET', 'http://localhost:56608/search/' + isbn, true);
+    xhr.send(null);
+
+    addBook({fetching: true, title: "Book with ISBN " + isbn, imageUrl: "https://loading.io/spinners/rolling/index.curve-bars-loading-indicator.gif"})
+    //setTimeout(() => { addBook({isbn: isbn, title: "newBook...."}) }, 3000)
+  }
+  const addBook = (bookJson) => {
+    setBooks(books.concat(bookJson));
+  }
+
+  const updateBook = (bookJson, index) => {
+    let updateContent = bookJson;
+    if (!bookJson.hasOwnProperty('title') && bookJson.hasOwnProperty('isbn')) {
+      updateContent = {title: "Cannot find book with ISBN " + bookJson.isbn, imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/65/Crystal_button_cancel.svg"}
+    }
+    books[index] = updateContent;
+    setBooks(books);
+  }
+  const removeBookAt = (index) => {
+    if (index >= 0) {
+      setBooks(books.slice(0, index).concat(books.slice(index+1)))
+    } else {
+      setBooks(books)   // No op
+    }
+  }
+
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return <AddressForm name={name} setName={setName} email={email} setEmail={setEmail} />;
+      case 1:
+        return <PaymentForm name={"TEST"} books={books} removeBookAt={removeBookAt} addBook={searchBook} fetching={fetching} />;
+      case 2:
+        return <Review />;
+      default:
+        throw new Error('Unknown step');
+    }
+  }
+
   return (
     <React.Fragment>
       <CssBaseline />
       <AppBar position="absolute" color="default" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
-            Company name
+            Bibli EFC
           </Typography>
         </Toolbar>
       </AppBar>
       <main className={classes.layout}>
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h4" align="center">
-            Checkout
+            Emprunt
           </Typography>
           <Stepper activeStep={activeStep} className={classes.stepper}>
             {steps.map(label => (
--- src/checkout/PaymentForm.js
+++ src/checkout/PaymentForm.js
@@ -4,38 +4,102 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Fab from '@material-ui/core/Fab';
+import Button from '@material-ui/core/Button';
+//import AddIcon from '@material-ui/icons/Add';
 
-export default function PaymentForm() {
+//import { makeStyles } from '@material-ui/core/styles';
+
+
+/*const useStyles = makeStyles(theme => ({
+  fab: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+const classes = useStyles();*/
+
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FormGroup from '@material-ui/core/FormGroup';
+//import FormControlLabel from '@material-ui/core/FormControlLabel';
+//import Checkbox from '@material-ui/core/Checkbox';
+//import Grid from '@material-ui/core/Grid';
+//import Typography from '@material-ui/core/Typography';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
+
+
+export default function PaymentForm(props) {
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+  const [writtenISBN, setISBN] = React.useState("9782070663859")
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Payment method
+        Ajouter un livre
       </Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardName" label="Name on card" fullWidth />
+        <Grid item xs={12} md={10}>
+          <TextField required id="isbn" label="ISBN" fullWidth onChange={(event) => setISBN(event.target.value)} value={writtenISBN}/>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardNumber" label="Card number" fullWidth />
+        <Grid item xs={12} md={2}>
+          <Button variant="contained" size="medium" color="primary" disabled={props.fetching} onClick={() => { props.addBook(writtenISBN); setISBN("") } } >
+            +
+          </Button>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="expDate" label="Expiry date" fullWidth />
+        <Grid item xs={12} md={12}>
+          <div className={classes.demo}>
+            <List dense={dense}>
+            { props.books.map(
+                (book,i) => <ListItem >
+                 
+                <ListItemAvatar>
+                  <Avatar src={book.imageUrl}>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText 
+                  primary={book.title}
+                  secondary={secondary ? 'Secondary text' : null}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="Delete" disabled={props.fetching} onClick={() => props.removeBookAt(i)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction> 
+                </ListItem>
+              )
+            }
+                  
+            </List>
+          </div>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
+        
       </Grid>
     </React.Fragment>
   );
